17ae83c42b8720924a4882646fd636e45a49b74b
Simon K Kamenov <simonkamenov7@gmail.com>
2020-11-11 00:02:36 +0200

    first javascript code

---
+++ games/counting-game/js/counting-game.js
@@ -0,0 +1,14 @@
+
+var countingGame = {};
+
+(function(){
+  var counter = 0;
+
+  countingGame.startGame1 = function(containerId){
+    console.log("starting game1 in container " + containerId);
+  };
+})();
+
+function sum(a, b, c){
+  return Number(a) + Number(b) + Number(c);
+};
