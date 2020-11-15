8fe6216d59f50baca6acb5acc14f7d87a2821f3c
Simon K Kamenov <simonkamenov7@gmail.com>
2020-11-15 23:20:25 +0200

    create counted and clickable elements in border field

--- games/counting-game/js/counting-game.js
+++ games/counting-game/js/counting-game.js
@@ -1,14 +1,61 @@
+/// <reference path="jquery-3.2.1.min.js" />
 
 var countingGame = {};
 
-(function(){
+(function($){
   var counter = 0;
+  var button = null;
+  var heading = null;
+  var gameField = null;
 
-  countingGame.startGame1 = function(containerId){
-    console.log("starting game1 in container " + containerId);
+  function sizeField(){
+    var headingHeight = $(heading).height();
+    var buttonHeight =
+     $(button).height() + parseInt($(button).css("padding-top")) + parseInt($(button).css("padding-bottom")) + 20; 
+    gameField.style.height = window.innerHeight - headingHeight - buttonHeight + "px";
+    gameField.style.top = headingHeight + "px";
+    gameField.style.position = "relative";
+   }
+
+   function increaseCount(){
+     counter += 1;
+     heading.innerHTML = counter;
+   }
+   
+   $(window).resize(sizeField);
+
+  countingGame.startGame = function(containerId){
+    counter = 0
+    var container = document.getElementById(containerId);
+    button = container.getElementsByTagName("button")[0];
+    heading = container.getElementsByClassName("score")[0];
+    gameField = document.createElement("div");
+    container.appendChild(gameField);
+    gameField.className = "game-field";
+    sizeField();
+    
+    var countables = [];
+    var gameFieldHeight = $(gameField).height();
+    var gameFieldWidth = $(gameField).width();
+    var countableSize = Math.min(gameFieldHeight, gameFieldWidth) / 20;
+    
+    for(var i=0; i<10; i+=1){
+      var countable = document.createElement("div");
+      gameField.appendChild(countable);
+      countable.style.height = countableSize + "px";
+      countable.style.width = countableSize + "px";
+      countable.style.border = "1px solid blue";
+      countable.style.top = i * countableSize * 2 + "px";
+      countable.style.left = i * countableSize * 2 + "px";
+      countable.style.position = "absolute";
+      $(countable).click(function(){
+      $(this).css("background", "green");
+      increaseCount();
+      });
+    
+   
+    }
+    
   };
-})();
 
-function sum(a, b, c){
-  return Number(a) + Number(b) + Number(c);
-};
+})(jQuery);
