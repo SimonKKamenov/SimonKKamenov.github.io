629acf1d4c6e3d7fad0433f3438b5367fd214dfd
Simon K Kamenov <simonkamenov7@gmail.com>
2021-01-10 19:24:32 +0200

    paint with erase and save button

--- games/paintDraw/js/paint-draw.js
+++ games/paintDraw/js/paint-draw.js
@@ -1,10 +1,34 @@
-/// <reference path="jquery-3.2.1.min.js" />
-var paintDraw = {};
 
-(function($) {
-  paintDraw.start = function(countainerId){
-    console.log('dada' + countainerId);
-  };
+    function _(selector){
+      return document.querySelector(selector);
+    }
+    function setup(){
+      let canvas = createCanvas(650, 600);
+      canvas.parent("canvas-wrapper");
+      background(255);
+    }
+    function mouseDragged(){
+      let type = _("#pen-pencil").checked?"pencil":"brush";
+      let size = parseInt(_("#pen-size").value);
+      let color = _("#pen-color").value;
+      strokeWeight(size);
 
-})(jQuery);
+      if(type == "pencil"){
+        fill(color);
+        stroke(color); 
+      } 
+      else {
+        fill('#ffffff');
+        stroke('#ffffff');
+      } 
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    }
+    _("#reset-canvas").addEventListener("click", function(){
+      background(255);
+    });
+    _("#save-canvas").addEventListener("click",function(){
+      saveCanvas(canvas, "paintDemo", "png");
+    });
+    
+  
 
